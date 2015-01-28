1aa48876b7c3f8132a626f6c8e524bce4ac02e8e
Jorge García-Luengo <jorgegarcialuengo@gmail.com>
2015-01-28 11:06:51 -0400

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,37 +10,80 @@ class MoviesController < ApplicationController
   def index
     @movies =[] 
     @chosenRatings =[] 
-
     @all_ratings = Movie.allRatings
+ 
+   
+    
+    if !params[:ratings] # dindt hit params, so its the first session or taped in orderBy
+      
+      if (params[:orderBy])
+        #raise params[:ratings].inspect
+        @movies = Movie.find(:all, :order=>params[:orderBy], :conditions=>{:rating => session[:ratings].keys})
+        session[:orderBy] = params[:orderBy]
+        @chosenRatings = session[:ratings].keys
+     
+      elsif session[:ratings]  
+        # raise params[:ratings].inspect
+        flash.keep
+        @chosenRatings = session[:ratings].keys
+        redirect_to movies_path(:sort => params[:orderBy], :ratings => session[:ratings])
 
-    if params.key?(:ratings)
-      # raise params[:ratings].keys.inspect
-      session[:ratings] = params[:ratings].keys
-    else
-      flash.keep
+      else # firt session
+        #raise params[:ratings].inspect
 
-    end  
+        @movies = Movie.all
+        @chosenRatings = Movie.allRatings
+        session[:ratings] = Movie.allRatings
 
+      end
 
-    if params[:orderBy] == 'releaseDate'
+    else # params[:ratings]
+      #raise params[:ratings].inspect
+      session[:ratings] = params[:ratings]
+      session[:orderBy] = params[:orderBy]
+      @movies = Movie.find(:all, :order=>params[:orderBy], :conditions=>{:rating => session[:ratings].keys})
+      @chosenRatings = session[:ratings].keys
+
+    end
+    
+      
+     # if session[:ratings] 
+       # raise params[:ratings].inspect
+
+      #  @movies
+       # flash.keep
+        #@chosenRatings = session[:ratings]
+        #redirect_to movies_path #(:sort => params[:orderBy], :rating => @chosenRatings)
+      #else # there is no session, first entry
+
+      #end
+     
+    #else # params[:ratings] is not empty
+    # raise params[:ratings].inspect 
+     # session[:ratings] = params[:ratings].keys
+      #@movies = Movie.find(:all, :order=>session[:orderBy], :conditions=>{:rating => session[:ratings]})
+      #@chosenRatings = params[:ratings].keys  
+    #end 
+
+   # if params[:orderBy] == 'releaseDate'
       # sort by date
       #@movies = Movie.order(:release_date)
-      @movies = Movie.find(:all, :order=>:release_date, :conditions=>{:rating => session[:ratings]})
-      @chosenRatings = session[:ratings]
-    elsif params[:orderBy] == 'title'
+    #  @movies = Movie.find(:all, :order=>params[:orderBy, :conditions=>{:rating => session[:ratings]})
+     # @chosenRatings = session[:ratings]
+    #elsif params[:orderBy] == 'title'
       # sort by title
       # @movies = Movie.order(:title)
-      @movies = Movie.find(:all, :order=>:title, :conditions=>{:rating => session[:ratings]})
-      @chosenRatings = session[:ratings]
-    elsif session[:ratings]
-      session[:ratings].each {|rate| 
-      @movies = @movies + Movie.where(rating: rate)
-      @chosenRatings = @chosenRatings + [rate]}
+     # @movies = Movie.find(:all, :order=>:title, :rating => session[:ratings])
+      #@chosenRatings = session[:ratings]
+    # elsif session[:ratings]
+      # session[:ratings].each {|rate| 
+      # @movies = @movies + Movie.where(rating: rate)
+      # @chosenRatings = @chosenRatings + [rate]}
 
-    else # nitial entry
-      @movies = Movie.all
-      @chosenRatings = Movie.allRatings
-    end
+  #  else # nitial entry
+   #   @movies = Movie.all
+    #  @chosenRatings = Movie.allRatings
+   # end
    	
   end
 
